9628c848ed405b8f52efe314a22eb7875afcc814
DESKTOP-PCSQO6M\hp-kyrylchenko <lampandroid@gmail.com>
2021-04-14 17:08:31 +0300

    123

--- Program.cs
+++ Program.cs
@@ -1,19 +1,8 @@
 class Program
 {
     static void Func()
-    {
-        while (true)
-        {
-            System.Console.WriteLine("Ku");
-        }
-        int[] arr = null;
-        Student st = new Student();
-    }
-
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello world!");
-        Console.WriteLine("зайчиков не будет!");
+    {("Hello world!");
+        Console.WriteLine("");
     }
 }
 
