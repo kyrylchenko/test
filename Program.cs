6ed723e6baacf290b791a68ed4999a96e9dd7135
DmitryChumak <chumakde@gmail.com>
2021-04-14 16:25:39 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 10; i++)
+        while (true)
         {
             System.Console.WriteLine("Ku");
         }
-        System.Console.WriteLine("End of program");
+        int [] arr = null;
     }
 }
