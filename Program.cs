46dc3bcfa6e44f718686caf8c809034ada7761db
LAN\student <student@itstep.org>
2021-04-14 16:26:46 +0300

    Func

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 class Program
 {
-    static void Main(string[] args)
+    static void Func()
     {
         for (int i = 0; i < 10; i++)
         {
             System.Console.WriteLine("Ku");
         }
         System.Console.WriteLine("End of program");
+    }
+    static void Main(string[] args)
+    {
+        Func();
     }
 }
